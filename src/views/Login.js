417fcfbc71b9b185e4e8ca6d413a413043bf3e31
davidvm1 <david.vvmm1997@gmail.com>
2020-10-06 17:05:38 -0500

    Login and Navbar added

---
+++ src/views/Login.js
@@ -0,0 +1,79 @@
+import { Button, Card, TextField } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  login: {
+    height: "60vh",
+    display: "flex",
+    justifyContent: "center",
+  },
+  login__card: {
+    padding: "20px",
+    width: "400px",
+    height: "60%",
+    margin: "auto",
+    marginTop: "60px",
+    justifyContent: "center",
+    backgroundColor: "#EEEEEE",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  login__form: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form__input: {
+    margin: theme.spacing(2),
+    width: "90%",
+    backgroundColor: "white",
+    color: "black",
+    borderRadius: "50px",
+    [`& fieldset`]: {
+      borderRadius: "50px",
+    },
+  },
+  form__button: {
+    minWidth: "40%",
+    minHeight: " 40px",
+    backgroundColor: "#00575A",
+    color: "white",
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const Login = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.login__card} boxShadow={0}>
+      <h2>Login</h2>
+      <form autoComplete="off" className={classes.login__form}>
+        <TextField
+          className={classes.form__input}
+          label="Email"
+          variant="outlined"
+          type="email"
+        />
+        <TextField
+          className={classes.form__input}
+          label="Password"
+          variant="outlined"
+          type="password"
+        />
+        <Button
+          className={classes.form__button}
+          variant="contained"
+          color="#006159"
+          type="submit"
+        >
+          Login
+        </Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
